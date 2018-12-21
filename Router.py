aa0844b6974a0e26f0d2cf12f221b2feaae6a713
Asmer <asmer.amenar@gmail.com>
2018-12-21 19:17:51 +0200

    "Router class added."

---
+++ Router.py
@@ -0,0 +1,17 @@
+class Router():
+    def __init__(self):
+        self.name               #string
+        self.interface          #set of dict
+        self.no_of_interfaces   #int
+        self.table              #set of dict
+    #end __init__
+
+    def update_table(self):
+
+    #end update_table
+
+    def print_table(self):
+
+    #end print_table
+
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+from . import Router
+
+
+class main():
+
+    routers

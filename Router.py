756de3d1211e9abdb7771011d446685232a40b98
Mohamed Gaber <mohamedgaber1452@gmail.com>
2018-12-21 21:08:19 +0200

    Merge branch 'master' of https://github.com/AsmerAmen/Network-Routing-Dijkstra

--- Router.py
+++ Router.py
@@ -1,34 +1,37 @@
-from collections import deque, namedtuple
+#from collections import deque, namedtuple
+import queue
+from collections import namedtuple
 
 
-inf = float('inf')
-Edge = namedtuple('Edge', 'start, end, cost')
+Edge = namedtuple('Edge', ['vertex', 'weight'])
 
 class Router():
     def __init__(self):
         self.name               #string
-        self.interface          #set of dict
         self.neighbors          #set of dict
+        self.interfaces          #set of dict
         self.table              #set of dict
     #end __init__
+    pass
 
 
-    def set_name(name):
+
+    def set_name(self,_name):
         self.name=name
         pass
 
-    def set_interface(self):
+    def set_interface(self,_interface):
         self.interface=interface
         pass
 
 
 
     def update_table(self):
+
         pass
     #end update_table
 
 
-
     def print_table(self):
         pass
-    #end print_tabl
+    #end print_table
--- main.py
+++ main.py
@@ -2,5 +2,104 @@ from . import Router
 
 
 class main():
+    def __init__(self):
+        self.routers = set()        #set of Router
 
-    
+        self.vertex_count = vertex_count
+        self.adjacency_list = [[] for _ in range(vertex_count)]
+    #end __init__
+
+
+    def add_router(self, _name, _interfaces):
+        '''
+            @param: name, string
+            @param: interfaces, set of Interface objects
+        '''
+        _router = Router()
+        _router.set_name(_name)
+        _router.set_interfaces(_interfaces)
+
+
+
+    #end add_router
+
+    def remove_router(self):
+        pass
+    #end remove_router
+
+
+    def get_edge(self, vertex):
+        for e in self.adjacency_list[vertex]:
+            yield e
+
+    def get_vertex(self):
+        for v in range(self.vertex_count):
+            yield v
+
+    def find_route(self, source, dest):
+
+        q = queue.PriorityQueue()
+        parents = []
+        distances = []
+        start_weight = float("inf")
+
+        for i in graph.get_vertex():
+
+            weight = start_weight
+            if source == i:
+                weight = 0
+            distances.append(weight)
+            parents.append(None)
+
+        q.put(([0, source]))
+
+        while not q.empty():
+            v_tuple = q.get()
+            v = v_tuple[1]
+
+            for e in graph.get_edge(v):
+
+                candidate_distance = distances[v] + e.weight
+                if distances[e.vertex] > candidate_distance:
+
+                    distances[e.vertex] = candidate_distance
+                    parents[e.vertex] = v
+                    # primitive but effective negative cycle detection
+                    if candidate_distance < -1000:
+
+                        raise Exception("Negative cycle detected")
+                    q.put(([distances[e.vertex], e.vertex]))
+
+        shortest_path = []
+        end = dest
+        while end is not None:
+
+            shortest_path.append(end)
+            end = parents[end]
+
+        shortest_path.reverse()
+
+        return shortest_path, distances[dest]
+        pass
+    #end find_route
+
+    def refresh(self):
+        pass
+    #end refresh
+
+    def add_end_user(self, _name, _interfaces):
+        '''
+            @param: name, string
+            @param: interfaces, set of Interface objects
+        '''
+        _end_user = End_User()
+        _end_user.set_name(_name)
+        _end_user.set_interfaces(_interfaces)
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()

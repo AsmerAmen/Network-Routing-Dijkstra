133b874b35a0cf48d85b5af372782b82119c84c7
Asmer <asmer.amenar@gmail.com>
2018-12-22 22:01:33 +0200

    "GUI add router, refresh, Table."

--- Interfaces.py
+++ Interfaces.py
@@ -1,27 +1,33 @@
 class Interface():
-    def __init__(self, _name, _neighbour, _ip, _cost, _subnet):
+    def __init__(self, _name, _neighbor, _ip, _cost, _subnet):
         self.name       = _name         # str
-        self.neighbour  = _neighbour    # str
+        self.neighbor  = _neighbor    # str
         self.ip         = _ip           # string
         self.cost       = _cost         # int
         self.subnet     = _subnet       # string
 
         self.network = self.get_network()   # network dict
+        # print(self.name+" "+ self.neighbor+" "+self.network)
     #end __init__
 
     def get_network(self):
         _ip_octets = list()
         _subnet_octets = list()
         _network_octets = list()
+
         for octet in self.ip.split('.'):
             _ip_octets.append(int(octet))
         #end for
+
         for octet in self.subnet.split('.'):
             _subnet_octets.append(int(octet))
         #end for
 
         for i in range(len(_subnet_octets)):
-            _network_octets.insert(i, (bin(_ip_octets[i])[2:]) and (bin(_subnet_octets[i])[2:]))
+            octet = (int(_ip_octets[i]) & int(_subnet_octets[i]))
+            _network_octets.insert(i,  str(octet))
         #end for
+
         _netowrk = _network_octets[0] + "." + _network_octets[1] + "." + _network_octets[2] + "." + _network_octets[3]
+
         return (_netowrk)
--- Router.py
+++ Router.py
@@ -10,6 +10,7 @@ Routing_Table_scheme={
     "interf_name"   : None,   #interface name : string
     "connected"     : None,     #if router is directly connected :bool
     "_interface"    : None,     #interface : interface object no printed
+    "subnet"        : None,
     "network"       : None,     #interface network : string
     "cost"          : None,     #interface cost : int
     "router"        : None,     #router name : string
@@ -19,12 +20,15 @@ class Router():
     def __init__(self, _name, _interfaces):
         self.name       = _name              #string
         self.interfaces = _interfaces         #list of dict
-        self.neighbors          #list of dict
+        self.neighbors = list()                 #list of dict
 
         self.table = list()              #list of dict
-        self.create_table
+        self.create_table()
+        print(self.name)
+        for interface in self.interfaces:
+            print(interface.name + interface.network)
     #end __init__
-    pass
+
 
 
     def get_name(name):
@@ -44,8 +48,9 @@ class Router():
             "connected"     : True,     #if router is directly connected :bool
             "_interface"    : interface,     #interface : interface object no printed
             "network"       : interface.network,     #interface network : string
+            "subnet"        : interface.subnet,
             "cost"          : interface.cost,     #interface cost : int
-            "router"        : interface.neighbour,     #router name : string
+            "router"        : interface.neighbor,     #router name : string
             }
             self.table.append(_table_row)
     #end update_table
@@ -53,6 +58,11 @@ class Router():
     def update_table(self):
 
         pass
+
+    def know_neighbors(self):
+        for interface in self.interfaces:
+            self.neighbors.append(interface.neighbor)
+
 
 
     def print_table(self):
